628dcb0cc082fbe00a06e2d1332cb72f1f45b72d
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 19:30:58 +0300

    Update push_to_dokerhub.groovy

--- vars/push_to_dokerhub.groovy
+++ vars/push_to_dokerhub.groovy
@@ -4,11 +4,11 @@ def call(String credentials) {
     script {
         withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) { 
             sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
-//            echo "Logging in to Docker Hub as: $DOCKER_USERNAME"
-//            sh "docker tag ${image_to_tag} ${dockerImage}"
-//            sh "docker push ${dockerImage}"
-          echo "docker tag: ${image_to_tag} new image name: ${dockerImage}"  
-          echo "The image has been pushed to DockerHub successfully!!"
+
+            sh "docker tag ${image_to_tag} ${dockerImage}"
+            sh "docker push ${dockerImage}"
+            echo "docker tag: ${image_to_tag} new image name: ${dockerImage}"  
+            echo "The image has been pushed to DockerHub successfully!!"
         }
     }
 }
