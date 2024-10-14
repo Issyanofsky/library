bc38259fa804575eb11c68417bf35baae4a65212
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 17:06:07 +0300

    Update push_to_dokerhub.groovy

--- vars/push_to_dokerhub.groovy
+++ vars/push_to_dokerhub.groovy
@@ -1,13 +1,14 @@
-def call( String credentials){ 
-  // takes credentials and the image name from the pipline variable (DOCKER_IMAGE)
-  String dockerImage = env.DOCKER_IMAGE
-  script {
-    withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-      sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+def call(String credentials) { 
+// takes credentials and the image name from the pipline variable (DOCKER_IMAGE)
+    String dockerImage = env.DOCKER_IMAGE 
+    script {
+        withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) { 
+            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
                         
-      sh "docker tag devopstasksupdated_app ${dockerImage}"
-      sh "docker push ${dockerImage}"
-     
+            sh "docker tag devopstasksupdated_app ${dockerImage}"
+            sh "docker push ${dockerImage}"
+            
+            sh 'echo "The image has been pushed to DockerHub successfully!!"'
+        }
     }
-  }
 }
