053150c52804e6fd6f767a114837a7634edd0bd5
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 16:39:32 +0300

    Create push_to_dokerhub.groovy

---
+++ vars/push_to_dokerhub.groovy
@@ -0,0 +1,11 @@
+def call(String dockerImage, String cresentials){
+  script {
+    withCredentials([usernamePassword(credentialsId: 'cresentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+      sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+                        
+      sh "docker tag devopstasksupdated_app ${dockerImage}"
+      sh "docker push ${dockerImage}"
+      sh 'echo "The image as pushed to DockerHub Successfuly!!"'
+    }
+  }
+}
