8ed08f19ad74b6f21246de56f82c44bd7f7c1e48
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 16:57:39 +0300

    Update push_to_dokerhub.groovy

--- vars/push_to_dokerhub.groovy
+++ vars/push_to_dokerhub.groovy
@@ -1,7 +1,7 @@
 def call( String cresentials){ // takes credentials and the image name from the pipline variable (DOCKER_IMAGE)
   String dockerImage = env.DOCKER_IMAGE
   script {
-    withCredentials([usernamePassword(credentialsId: 'cresentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: cresentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
       sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
                         
       sh "docker tag devopstasksupdated_app ${dockerImage}"
