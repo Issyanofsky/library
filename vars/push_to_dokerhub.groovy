5bea1e0cb542457f41e1e46444f0b4595bec424e
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 17:00:08 +0300

    Update push_to_dokerhub.groovy

--- vars/push_to_dokerhub.groovy
+++ vars/push_to_dokerhub.groovy
@@ -1,4 +1,5 @@
-def call( String credentials){ // takes credentials and the image name from the pipline variable (DOCKER_IMAGE)
+def call( String credentials){ 
+  // takes credentials and the image name from the pipline variable (DOCKER_IMAGE)
   String dockerImage = env.DOCKER_IMAGE
   script {
     withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
