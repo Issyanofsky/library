c26741f3a1d871fc51f412560ea3ce0180ee1508
Issyanofsky <161239203+Issyanofsky@users.noreply.github.com>
2024-10-14 19:25:39 +0300

    Update push_to_dokerhub.groovy

--- vars/push_to_dokerhub.groovy
+++ vars/push_to_dokerhub.groovy
@@ -1,17 +1,14 @@
 def call(String credentials) { 
-    echo "Starting Docker push process..."
     String dockerImage = env.DOCKER_IMAGE
     String image_to_tag = env.IMAGE_NAME 
-    echo "Using Docker cred: "
-    echo credentials
     script {
-//        withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) { 
-//            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+        withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) { 
+            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
 //            echo "Logging in to Docker Hub as: $DOCKER_USERNAME"
 //            sh "docker tag ${image_to_tag} ${dockerImage}"
 //            sh "docker push ${dockerImage}"
             
           echo "The image has been pushed to DockerHub successfully!!"
-//        }
+        }
     }
 }
